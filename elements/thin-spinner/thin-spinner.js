759749fa47a578424f79b0feda1af83ab2a18d80
heymp <mgp140@gmail.com>
2020-07-14 10:53:34 -0400

    added thin-spinner

---
+++ elements/thin-spinner/thin-spinner.js
@@ -0,0 +1,34 @@
+import { html, css } from "lit-element/lit-element.js";
+import { CleanOne } from "@lrnwebcomponents/clean-one/clean-one.js";
+
+class HaxcmsBlankTheme extends CleanOne {
+  static get properties() {
+    return {
+      ...super.properties
+    };
+  }
+  constructor() {
+    super();
+  }
+  //styles function
+  static get styles() {
+    return [css``];
+  }
+  static get tag() {
+    return "thin-spinner";
+  }
+  // render function
+  render() {
+    return html`
+      <div id="contentcontainer">
+        <div id="slot">
+          <slot></slot>
+        </div>
+      </div>
+    `;
+  }
+}
+
+customElements.define(HaxcmsBlankTheme.tag, HaxcmsBlankTheme);
+
+export { HaxcmsBlankTheme };
