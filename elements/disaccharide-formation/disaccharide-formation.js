37f988e7946fbec4df55b813dc58a9c9b09f9109
heymp <mgp140@gmail.com>
2020-08-19 13:15:51 -0400

    added disaccharide formation

---
+++ elements/disaccharide-formation/disaccharide-formation.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2020 PSU
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { LitElement, html, svg } from "lit-element/lit-element.js";
+import "@eberlywc/eberlywc-animationctl-button/eberlywc-animationctl-button.js";
+import "@eberlywc/maltose-element/maltose-element.js";
+import "@eberlywc/sucrose-element/sucrose-element.js";
+
+/**
+ * `disaccharide-formation`
+ * ``
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @lit-html
+ * @lit-element
+ * @demo demo/index.html
+ */
+class DisaccharideFormation extends LitElement {
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  tag() {
+    return "disaccharide-formation";
+  }
+
+  static get properties() {
+    return {
+    };
+  }
+
+  // life cycle
+  constructor() {
+    super();
+  }
+
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  render() {
+    return html`
+      <maltose-element></maltose-element>
+      <sucrose-element></sucrose-element>
+    `;
+  }
+}
+customElements.define("disaccharide-formation", DisaccharideFormation);
+
+export { DisaccharideFormation };
---
+++ elements/disaccharide-formation/disaccharide-formation.story.js
@@ -0,0 +1,57 @@
+import { storiesOf } from "@storybook/polymer";
+import * as storybookBridge from "@storybook/addon-knobs/polymer";
+import { DisaccharideFormation } from "./disaccharide-formation.js";
+import "@polymer/iron-demo-helpers/demo-snippet.js";
+// need to account for polymer goofiness when webpack rolls this up
+var template = require("raw-loader!./demo/index.html");
+let pattern = /<body[^>]*>((.|[\n\r])*)<\/body>/im;
+var array_matches = pattern.exec(template);
+// now template is just the body contents
+template = array_matches[1];
+const stories = storiesOf("Element", module);
+stories.addDecorator(storybookBridge.withKnobs);
+stories.add("disaccharide-formation", () => {
+  var binding = {};
+  // start of tag for demo
+  let elementDemo = `<disaccharide-formation`;
+  // mix in properties defined on the class
+  for (var key in DisaccharideFormation.properties) {
+    // skip prototype
+    if (!DisaccharideFormation.properties.hasOwnProperty(key)) continue;
+    // convert typed props
+    if (DisaccharideFormation.properties[key].type.name) {
+      let method = "text";
+      switch (DisaccharideFormation.properties[key].type.name) {
+        case "Boolean":
+        case "Number":
+        case "Object":
+        case "Array":
+        case "Date":
+          method = DisaccharideFormation.properties[key].type.name.toLowerCase();
+          break;
+        default:
+          method = "text";
+          break;
+      }
+      binding[key] = storybookBridge[method](
+        key,
+        DisaccharideFormation.properties[key].value
+      );
+      // ensure ke-bab case
+      let kebab = key.replace(/[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g, function(
+        match
+      ) {
+        return "-" + match.toLowerCase();
+      });
+      elementDemo += ` ${kebab}="${binding[key]}"`;
+    }
+  }
+  const innerText = storybookBridge.text("Inner contents", "Element");
+  elementDemo += `> ${innerText}</disaccharide-formation>`;
+  return `
+  <h1>Live demo</h1>
+  ${elementDemo}
+  <h1>Additional examples</h1>
+  ${template}
+  `;
+});
