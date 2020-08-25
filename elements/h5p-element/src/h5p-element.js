9c2ea34948d510e93af383034172ec28cfbfe5ab
heymp <mgp140@gmail.com>
2020-08-25 12:01:52 -0400

    added h5p element

---
+++ elements/h5p-element/src/h5p-element.js
@@ -0,0 +1,131 @@
+import { store } from "@lrnwebcomponents/haxcms-elements/lib/core/haxcms-site-store.js";
+import { FlattenedNodesObserver } from "@polymer/polymer/lib/utils/flattened-nodes-observer.js";
+import { css, html, LitElement } from "lit-element/lit-element.js";
+import { autorun } from "mobx/lib/mobx.module.js";
+import "@eberlywc/iframe-loader/iframe-loader.js";
+
+class H5pElement extends LitElement {
+  static get properties() {
+    return {
+      __editLink: { type: String, attribute: false },
+      __editMode: { type: Boolean, attribute: false }
+    };
+  }
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      [part="container"] {
+        display: block;
+        position: relative;
+      }
+      .editing[part="container"] {
+        min-height: 100px;
+      }
+      [part="edit-screen"] {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: flex-end;
+        justify-content: flex-end;
+      }
+      [part="source-link"] a {
+        background: black;
+        color: white;
+        padding: 1em;
+      }
+      [part="anchor"] {
+        display: block;
+      }
+    `;
+  }
+  constructor() {
+    super();
+    this.src = "";
+    this.editMode = store.editMode;
+  }
+  connectedCallback() {
+    super.connectedCallback();
+    this.addEventListener("click", this.__clickHandler.bind(this));
+    this.__disposer = autorun(() => {
+      this.__editMode = store.editMode;
+    });
+  }
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    this.__disposer();
+  }
+  firstUpdated() {
+    this._observer = new FlattenedNodesObserver(this, info => {
+      info.addedNodes.forEach(item => {
+        const iframe =
+          item.nodeName === "IFRAME" ? item : this.querySelector("iframe");
+        if (iframe) {
+          if (typeof iframe.src !== "undefined") {
+            const editUrl = "https://media.ed.science.psu.edu/node/$1/edit"
+            this.__editLink = iframe.src.replace(/.*\/node\/([0-9]*)/, editUrl)
+          }
+        }
+      });
+    });
+  }
+  updated(changedProps) {
+    changedProps.forEach((old, prop) => {
+      if (prop === "src") {
+        if (this.src) {
+        }
+      }
+    })
+  }
+  static get haxProperties() {
+    return {
+      canScale: false,
+      canPosition: true,
+      canEditSource: false,
+      gizmo: {
+        title: "H5P Element",
+        description: "Add an H5P iframe embed.",
+        icon: "icons:file-download",
+        color: "blue",
+        meta: {
+          author: "LRNWebComponents"
+        }
+      },
+      settings: {
+        configure: [
+          {
+            slot: "",
+            title: "iframe",
+            description: "The html code of the iframe",
+            inputMethod: "code-editor",
+            icon: "editor:iframe"
+          }
+        ],
+        advanced: []
+      }
+    };
+  }
+  render() {
+    return html`
+      <div part="container" class="${this.__editMode ? 'editing' : 'editing' }">
+        ${this.__editMode
+          ? html`
+              <slot></slot>
+              <div part="edit-screen">
+                <div part="source-link"><a part="anchor" href="${this.__editLink}" target="_blank">Edit H5P Source</a></div>
+              </div>
+            `
+          : html`
+            <iframe-loader><slot></slot></iframe-loader>
+          `}
+      </div>
+    `;
+  }
+
+  __clickHandler() {}
+}
+customElements.define("h5p-element", H5pElement);
---
+++ elements/iframe-loader/iframe-loader.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2020 The Pennsylvania State University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { html, css } from "lit-element/lit-element.js";
+import { CleanTwo } from "@lrnwebcomponents/clean-two/clean-two.js";
+
+/**
+ * `clean-two`
+ * `A 2nd clean theme`
+ *
+ * @microcopy - language worth noting:
+ *  - HAXcms - A headless content management system
+ *  - HAXCMSTheme - A super class that provides correct baseline wiring to build a new theme
+ *
+ * @demo demo/index.html
+ * @element clean-two
+ */
+class IframeLoader extends CleanTwo {
+  //styles function
+  static get styles() {
+    return [
+      ...super.styles,
+      css`
+        site-menu {
+          margin: 0;
+        }
+        .content {
+          margin: 16px;
+        }
+        .right-col {
+          display: block;
+          overflow: hidden;
+        }
+      `
+    ];
+  }
+  constructor() {
+    super();
+  }
+  static get tag() {
+    return "iframe-loader";
+  }
+}
+window.customElements.define(IframeLoader.tag, IframeLoader);
+export { IframeLoader };
